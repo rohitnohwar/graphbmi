94b57c63ad9c0e179e8e86fc8d282552a0ad57fa
rohit <rohit.nohwar@gmail.com>
2022-06-02 16:28:27 +0530

    jhnaq

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Main from "./Main";
-import Graph from "./Graph";
+// import Main from "./Main";
+// import Graph from "./Graph";
+const Main = React.lazy(() => import('./Main.js'));
+const Graph = React.lazy(() => import('./Graph.js'));
+
+// function App() {
+//   return (
+//     <Router>
+//   <div>
+//   <Switch>
+//     <Route path="/" exact component={Main} />
+//     <Route path="/graph" exact component={Graph} />
+//   </Switch>
+//   </div>
+//   </Router>
+//   );
+// }
 
 function App() {
   return (
     <Router>
   <div>
+  <Suspense fallback={<div style={{margin:'auto', textAlign:'center',fontSize:'30px', fontWeight:'800'}}>Loading...</div>}>
   <Switch>
     <Route path="/" exact component={Main} />
     <Route path="/graph" exact component={Graph} />
   </Switch>
+  </Suspense>
   </div>
   </Router>
   );
